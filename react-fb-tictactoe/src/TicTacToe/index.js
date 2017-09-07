93297f922c6ae7833ab3b1b3f3575f580c7826e1
Edgardo <medgardo@gmail.com>
2017-09-06 22:22:32 -0400

    Complete: Declaring a winner

--- react-fb-tictactoe/src/TicTacToe/index.js
+++ react-fb-tictactoe/src/TicTacToe/index.js
@@ -22,6 +22,9 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -39,7 +42,13 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -68,7 +77,7 @@ class Game extends Component {
   render() {
     return (
       <div>
-        <h3>Tutorial Step: Taking turns</h3>
+        <h3>Tutorial Step: Declaring a Winner</h3>
         <div className="game">
           <div className="game-board">
             <Board />
@@ -81,6 +90,29 @@ class Game extends Component {
       </div>
     );
   }
+}
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] 
+        && squares[a] === squares[b] 
+        && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Game;
