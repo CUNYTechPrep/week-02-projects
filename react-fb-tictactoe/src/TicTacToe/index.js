7fb962c380f37728d16fc74b08eb880204a6a0bd
Maria Romanova <aemulare@users.noreply.github.com>
2017-09-11 22:14:05 -0400

    Step 2. An Interactive Component.

--- react-fb-tictactoe/src/TicTacToe/index.js
+++ react-fb-tictactoe/src/TicTacToe/index.js
@@ -10,10 +10,17 @@ import './TicTacToe.css'
  *    class Square extends Component
  */
 class Square extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+  
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
