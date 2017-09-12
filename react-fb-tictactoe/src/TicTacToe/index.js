bf22db8289dc3593dc967b0e9fd1f6fefe8cfecc
Maria Romanova <aemulare@users.noreply.github.com>
2017-09-11 23:00:35 -0400

    Step 8. Showing the Moves.

--- react-fb-tictactoe/src/TicTacToe/index.js
+++ react-fb-tictactoe/src/TicTacToe/index.js
@@ -85,6 +85,18 @@ class Game extends Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start';
+      return (
+        <li>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
+    
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -102,7 +114,7 @@ class Game extends Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
