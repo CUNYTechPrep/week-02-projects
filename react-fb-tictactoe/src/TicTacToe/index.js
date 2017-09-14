b66d3e5e3006df3bc1144b29e110f0ca12818469
Neal Rea <nealrea818@gmail.com>
2017-09-14 15:21:39 -0400

    create controlled components (squares) in Board

--- react-fb-tictactoe/src/TicTacToe/index.js
+++ react-fb-tictactoe/src/TicTacToe/index.js
@@ -10,15 +10,9 @@ import './TicTacToe.css'
  *    class Square extends Component
  */
 class Square extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value : null,
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => alert('Click')}>
+      <button className="square" onClick={() => this.props.onClick({value : 'X'})}>
         {this.props.value}
       </button>
     );
@@ -26,8 +20,26 @@ class Square extends Component {
 }
 
 class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
