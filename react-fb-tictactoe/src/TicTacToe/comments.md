1d9e0a1805bfa9c19871ba51d7bbc42378c558c5
David Hadaller <david.hadaller@macaulay.cuny.edu>
2017-09-15 14:38:12 -0400

    First Commit

---
+++ react-fb-tictactoe/src/TicTacToe/comments.md
@@ -0,0 +1,21 @@
+In the square class definition, under the render function, why is it that button has the prop className? Isn't it already in the class? Why that extra indicator? 
+-the reason the className="square" is in there is because that is what will be written to the virtual dom and eventually the dom and that tells teh css how to style the button we're working on. That the class/the component/the function in react.js has the same name is merely incedental.
+
+why isn't there a paragraph box or text box surrounding {this.state.value}? If that's where the "X" is supposed to appea -- actually nevermind, from w3schools it says that the correct syntax for a button is something like this:
+ <button type="button">Click Me!</button> 
+
+ the type attribute in the above exmple is a to indicate to the browser what type of button it is.
+
+why do you need to define render square twice--once in the square class and another time in the board component?
+
+My biggest question is with the handleclick thing. where is it defined what hapens once the button is clicked? the button calls its props and the onClick prop just returns a function "handleClick" which isn't very descriptive.
+never mind, this was detailed in the next step. a handle click function was the next thing I had to write.
+
+ah so in the handle click function sets the state listed in the (squares) with the local variable in the handle click function. for some reason for which I am not to certain, I can't change the state of the squares array in the board directly. Why is that?
+
+why can't I just write Board.state.squares[i]= 'X';? in the handle click function?
+
+removed square class because it only had a render function--replaced it instead witha a function that takes props as input and renders the square.
+
+ohhhh so the state is just a javascript object that gets periodically modified by the rest of your code!!!! I seeee. 
+
--- react-fb-tictactoe/src/TicTacToe/index.js
+++ react-fb-tictactoe/src/TicTacToe/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './TicTacToe.css'
 
-
 /*
  * Changes from original tutorial
  *  - Here we import Component, so we can replace
@@ -9,23 +8,53 @@ import './TicTacToe.css'
  *    with
  *    class Square extends Component
  */
-class Square extends Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -67,3 +96,23 @@ class Game extends Component {
 }
 
 export default Game;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
