07e2fd41133ff7f809e02f759b23e52be0db3925
Neal Rea <nealrea818@gmail.com>
2017-09-17 15:33:08 -0400

    add two player functionality

--- react-fb-tictactoe/src/TicTacToe/index.js
+++ react-fb-tictactoe/src/TicTacToe/index.js
@@ -33,13 +33,17 @@ class Board extends Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -52,7 +56,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
