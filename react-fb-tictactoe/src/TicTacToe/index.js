b291d79002dbac4ee16f6aa560294b2c858296f3
Maria Romanova <aemulare@users.noreply.github.com>
2017-09-17 18:25:41 -0400

    Step 12. Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- react-fb-tictactoe/src/TicTacToe/index.js
+++ react-fb-tictactoe/src/TicTacToe/index.js
@@ -30,25 +30,15 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    let rows = [];
+    for(let i = 0; i < 3; i++) {
+      let columns = [];
+      for(let j = 0; j < 3; j++) {
+        columns.push(this.renderSquare(i * 3 + j))
+      }
+      rows.push(<div className="board-row">{columns}</div>)
+    }
+    return (<div>{rows}</div>);
   }
 }
 
